d3ff2f1e6dd3dcd90e3578e04b3135df985b5526
eidde606 <eidde_606@yahoo.com>
2026-07-10 19:17:27 -0400

    Implement protected authentication flow with Zustand and React Router

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,12 +1,17 @@
 import {Routes, Route} from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import DashboardPage from "../pages/DashboardPage";
+import ProtectedRoute from "../components/ProtectedRoute.tsx";
 
 function AppRouter() {
     return (
         <Routes>
             <Route path="/login" element={<LoginPage/>}/>
-            <Route path="/dashboard" element={<DashboardPage/>}/>
+            <Route element={<ProtectedRoute/>}>
+                <Route path="/dashboard"
+                       element={<DashboardPage/>}/>
+
+            </Route>
         </Routes>
     )
 }
